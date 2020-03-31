b4ae51370d0ba3c0f109f9d2fa52e84510c9aa7a
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-03-30 19:16:05 -0500

    forgot to add PATH variable. Surely it should work

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname, + "/dist/"));
-app.get(/.*/, (request, response) => {
-    response.sendFile(__dirname + "/dist/index.html");
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, "dist/index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server started at port ${port}`);
+    console.log(`Serveur démarrer au port ${port}`);
 });
